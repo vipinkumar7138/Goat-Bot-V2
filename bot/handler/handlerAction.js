f35599d599894096d6f9f2343f22518730727431
vipinkumar7138 <ashish713914@gmail.com>
2025-08-16 15:21:20 +0530

    Add files via upload

---
+++ bot/handler/handlerAction.js
@@ -0,0 +1,84 @@
+const createFuncMessage = global.utils.message;
+const handlerCheckDB = require("./handlerCheckData.js");
+
+module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
+        const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
+
+        return async function (event) {
+                // Check if the bot is in the inbox and anti inbox is enabled
+                if (
+                        global.GoatBot.config.antiInbox == true &&
+                        (event.senderID == event.threadID || event.userID == event.senderID || event.isGroup == false) &&
+                        (event.senderID || event.userID || event.isGroup == false)
+                )
+                        return;
+
+                const message = createFuncMessage(api, event);
+
+                await handlerCheckDB(usersData, threadsData, event);
+                const handlerChat = await handlerEvents(event, message);
+                if (!handlerChat)
+                        return;
+
+                const {
+                        onAnyEvent, onFirstChat, onStart, onChat,
+                        onReply, onEvent, handlerEvent, onReaction,
+                        typ, presence, read_receipt
+                } = handlerChat;
+
+
+                onAnyEvent();
+                switch (event.type) {
+                        case "message":
+                        case "message_reply":
+                        case "message_unsend":
+                                onFirstChat();
+                                onChat();
+                                onStart();
+                                onReply();
+                                break;
+                        case "event":
+                                handlerEvent();
+                                onEvent();
+                                break;
+                        case "message_reaction":
+                                onReaction();
+
+                                //this is for kicking someone or removing bot. change the uid
+                                if (event.reaction == "💀") {
+                                        if (event.userID == "61556609578687") {
+                                                api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+                                                        if (err) return console.log(err);
+                                                });
+
+                                        } else {
+                                                return
+                                        }
+                                }
+                                //this is for removing messages. 
+                                if (event.reaction == "👍") {
+                                        message.unsend(event.messageID)
+                                }
+
+                                break;
+                        case "typ":
+                                typ();
+                                break;
+                        case "presence":
+                                presence();
+                                break;
+                        case "read_receipt":
+                                read_receipt();
+                                break;
+                        // case "friend_request_received":
+                        // { /* code block */ }
+                        // break;
+
+                        // case "friend_request_cancel"
+                        // { /* code block */ }
+                        // break;
+                        default:
+                                break;
+                }
+        };
+};
